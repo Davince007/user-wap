8c0e64db6a78da904af5dec01a08e2c68a09495c
GWD <gaoweidong127@163.com>
2017-11-18 01:21:21 +0800

    初始化工程

---
+++ src/main/java/com/nwpu/wap/UserWapApplication.java
@@ -0,0 +1,12 @@
+package com.nwpu.wap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserWapApplication.class, args);
+	}
+}
